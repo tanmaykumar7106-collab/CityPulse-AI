32194b4166a81548ef9fcd5d89c16becb1174db8
Tanmay Kumar <tanmaykumar7106@gmail.com>
2026-08-23 20:39:44 +0530

    chore: remove frontend from git tracking

--- frontend/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-    <footer className="bg-[#0B3D91] text-white py-8 text-center">
-        <p>© 2026 CityPulse AI. All rights reserved.</p>
-    </footer>
-    );
-}
-
-export default Footer;
